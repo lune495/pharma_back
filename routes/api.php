450a99be5dffb238901e12b9e0277accc384dc8b
Alioune badara dieng <badaralune9@gmail.com>
2022-09-06 18:46:43 +0100

    Update

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ Route::post('/login',[AuthController::class,'login']);
 Route::get('/produits/{id}',[ProduitController::class, 'show']);
 Route::get('/top_produit_vendu',[ProduitController::class, 'list']);
 Route::get('/produits/search/{name}',[ProduitController::class, 'search']);
+    Route::post('/ventes',[VenteController::class,'save']);
 
 
 // Protected Routes
@@ -36,7 +37,6 @@ Route::group(['middleware' => ['auth:sanctum']],function()
 {
     Route::post('/produits',[ProduitController::class,'save']);
     Route::post('/approsboutique',[MouvementController::class,'ravitaillerboutique']);
-    Route::post('/ventes',[VenteController::class,'save']);
     Route::post('/clients',[ClientController::class,'save']);
     Route::post('/fournisseurs',[FournisseurController::class,'save']);
     Route::post('/depots',[DepotController::class,'save']);
