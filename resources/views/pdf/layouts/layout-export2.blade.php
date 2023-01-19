5fffd22b51f723e3bad5953d706546f4dcd8ae17
Alioune badara dieng <badaralune9@gmail.com>
2023-01-19 16:55:31 +0100

    Updat

--- resources/views/pdf/layouts/layout-export2.blade.php
+++ resources/views/pdf/layouts/layout-export2.blade.php
@@ -139,7 +139,7 @@
 
             <header class = "mb-4"  style="width:100%;">
                 <div class="header text-center" style="font-size: 300px;font-weight:bold">
-                    <img style="width: 100%" src="{{'my_custom_symlink_1/Logo_saloum_digital.jpg'}}" alt="">
+                    <img style="width: 100%" src="{{asset('my_custom_symlink_1/logo.jpg')}}" alt="">
                 </div>
             </header>
 
@@ -148,27 +148,19 @@
             </div>
 
             <!--Footer page-->
-            {{-- <div class="footer">
-                 <div class="text-left" style="font-size:8px;">
-                    <p> Dans le cas où le paiement intégral n'interviandrait pas à la date prévue par les parties, 
-                        <br>le vendeur se réserve le droit de reprendre la livraison et de dissoudre le contrat.
-                        <br> En cas de paiement anticipé application d'un escompte de 60%.
-                    </p>
-                </div> 
+            <div class="footer">
                  <div style="font-size:9px;font-weight: bold; border: 1px solid black; padding: 5px ">
                     <p> 
-                        <i> Toute marchandise commandée, livrée et non payée dans son intégralité reste propriété exclusive de FILIALE. Et FILIALE se donne le droit de récupérer sa marchandise si celle-ci n’est pas régularisé dans son intégralité sous 30 jours maximum à compter de la date d’achat de la facture. 
+                        <i>La confiance n'exclut pas le controle, veuillez verifier votre facture a la réception
                         <br>
-                        Tout montant versé en acompte n’est pas remboursable et fait office d’engagement d’achat auprès de FILIALE et reste propriété de FILIALE.   
-                        </i> 
+                        {{-- Tout montant versé en acompte n’est pas remboursable et fait office d’engagement d’achat auprès de FILIALE et reste propriété de FILIALE.   
+                        </i>  --}}
                     </p>
-                </div> 
+                </div>
                 <hr>
                 <div style="display:inline-flex" style="font-size:10px;">
-                    <p class="text-left" style="font-size:10px;"> RC: SN DKR 2014 A 1270</p>
-                    <p class="text-right" style="font-size:10px;"> NINEA: 4982965 1A1 - Dakar</p> 
+                    <p class="text-left" style="font-size:10px;"> NINEA: 00455 90 26 - Dakar</p> 
                 </div>
-            </div> --}}
-           
+            </div>
         </body>
     </html>
