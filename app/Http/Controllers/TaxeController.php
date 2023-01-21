c44bcd9c5c33017c8b0956715a0cea63cfcc1853
Alioune badara dieng <badaralune9@gmail.com>
2023-01-21 14:52:43 +0100

    Update

--- app/Http/Controllers/TaxeController.php
+++ app/Http/Controllers/TaxeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\{Taxe};
+use App\Models\{Taxe,Outil};
 
 class TaxeController extends Controller
 {
