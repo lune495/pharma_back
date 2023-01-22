8d9173e4a65592166bad0d83a4f8f387d265ded5
Alioune badara dieng <badaralune9@gmail.com>
2023-01-22 22:38:51 +0100

    Update

--- resources/views/pdf/layouts/layout-export2.blade.php
+++ resources/views/pdf/layouts/layout-export2.blade.php
@@ -139,7 +139,7 @@
 
             <header class = "mb-4"  style="width:100%;">
                 <div class="header text-center" style="font-size: 300px;font-weight:bold">
-                    <img style="width: 100%" src="{{asset('Logo_saloum_digital.jpg')}}" alt="">
+                    <img style="width: 100%" src="http://localhost/laravel-app/storage/public/Logo_saloum_digital.jpg" alt="">
                 </div>
             </header>
             <div class="mt-4">
