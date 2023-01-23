8df4f93682c7b1760144ef1e38a3ee56a8fe3dd7
Alioune badara dieng <badaralune9@gmail.com>
2023-01-23 14:25:22 +0100

    Update

--- app/Http/Controllers/VenteController.php
+++ app/Http/Controllers/VenteController.php
@@ -41,10 +41,10 @@ class VenteController extends Controller
                 {
                     $item = Vente::find($request->id);
                 }
-                if (empty($item->client_id))
-                {
-                    $errors = "Renseignez le client";
-                }
+                // if (empty($item->client_id))
+                // {
+                //     $errors = "Renseignez le client";
+                // }
                 
                     DB::beginTransaction();
                     // $item->montantencaisse = $request->montantencaisse;
