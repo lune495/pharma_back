faa8e32cc8eda90952951b363cd38081174d5332
Alioune badara dieng <badaralune9@gmail.com>
2023-01-23 14:21:11 +0100

    Update

--- app/Http/Controllers/VenteController.php
+++ app/Http/Controllers/VenteController.php
@@ -50,7 +50,6 @@ class VenteController extends Controller
                     // $item->montantencaisse = $request->montantencaisse;
                     // $item->monnaie = $request->monnaie;
                     $item->client_id = $request->client_id;
-                    dd($request->client_id);
                     //$item->user_id = $user_id;
                     $item->user_id = 1;
                     $str_json = json_encode($request->details);
