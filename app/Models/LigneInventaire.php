f987f9f5b7e8c29e64d9da7c29182d22bcacd40e
lune495 <badaralune9@gmail.com>
2023-09-11 19:37:21 +0000

    Update

--- app/Models/LigneInventaire.php
+++ app/Models/LigneInventaire.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class LigneInventaire extends Model
 {
     use HasFactory;
+    public  function produit()
+    {
+        return $this->belongsTo(Produit::class);
+    }
 }
