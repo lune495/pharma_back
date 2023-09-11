73c9d083a4706389fd77a6c69cb9735f3e0d330e
lune495 <badaralune9@gmail.com>
2023-09-11 19:36:09 +0000

    Update

--- app/Models/Inventaire.php
+++ app/Models/Inventaire.php
@@ -9,6 +9,11 @@ class Inventaire extends Model
 {
     use HasFactory;
 
+    public  function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
     public  function ligne_inventaires()
     {
         return $this->hasMany(LigneInventaire::class, 'inventaire_id', 'id');
