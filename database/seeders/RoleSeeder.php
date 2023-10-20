cf3d72f3cd10fdad28ae340bb297c09d43c8e07f
lune495 <badaralune9@gmail.com>
2023-10-20 23:36:18 +0100

    Update

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use  Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use App\Models\{Role,Taxe};
+use App\Models\{Role,Taxe,LigneApprovisionnement};
 
 
 class RoleSeeder extends Seeder
@@ -16,21 +16,28 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        $taxes = array();
-        array_push($taxes,array("value" => "18"));
-        $newtaxe = Taxe::where('value', $taxes[0]['value'])->first();
-        if (!isset($newtaxe))
+        // $taxes = array();
+        // array_push($taxes,array("value" => "18"));
+        // $newtaxe = Taxe::where('value', $taxes[0]['value'])->first();
+        // if (!isset($newtaxe))
+        // {
+        //     $newtaxe = new Taxe();
+        //     $newtaxe->value = $taxes[0]['value'];
+        // }
+        // $newtaxe->save();
+        // $newrole = Role::where('nom','ADMIN')->first();
+        // if(!isset($newrole))
+        // {
+        //       DB::table('roles')->insert([
+        //         'nom'=>'ADMIN',
+        //       ]);
+        // } 
+        
+        $ligne_appros = LigneApprovisionnement::all();
+        foreach($ligne_appros as $ligne_appro)
         {
-            $newtaxe = new Taxe();
-            $newtaxe->value = $taxes[0]['value'];
-        }
-        $newtaxe->save();
-        $newrole = Role::where('nom','ADMIN')->first();
-        if(!isset($newrole))
-        {
-              DB::table('roles')->insert([
-                'nom'=>'ADMIN',
-              ]);
-        }   
+              $ligne_appro->pu = $ligne_appro->produit->pa;
+              $ligne_appro->save();
+        } 
     }
 }
