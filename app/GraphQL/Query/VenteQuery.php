f6db3202021bff6ec9f7d79d59845a1b33bbb965
lune495 <badaralune9@gmail.com>
2023-10-23 22:05:18 +0100

    Update

--- app/GraphQL/Query/VenteQuery.php
+++ app/GraphQL/Query/VenteQuery.php
@@ -5,7 +5,7 @@ namespace App\GraphQL\Query;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Query;
 use Rebing\GraphQL\Support\Facades\GraphQL;
-use App\Models\Vente;
+use App\Models\{Vente,ClotureCaisse};
 class VenteQuery extends Query
 {
     protected $attributes = [
@@ -64,6 +64,12 @@ class VenteQuery extends Query
             $to = date($to.' 23:59:59');
             $query->whereBetween('created_at', array($from, $to));
         }
+        $latestClosureDate = ClotureCaisse::orderBy('date_fermeture', 'desc')
+        ->value('date_fermeture');
+        if(isset($latestClosureDate))
+        {
+            $query = $query->whereBetween('created_at', [$latestClosureDate, now()]);
+        }  
         $query->orderBy('id', 'desc');
         $query = $query->get();
         return $query->map(function (Vente $item)
